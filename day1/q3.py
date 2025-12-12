a2ba80a746af2ecbb4ddf1d0a1be408d45a41824
Darshana kamble <23uet044@dkte.ac.in>
2025-12-12 19:12:53 +0530

    day1

---
+++ day1/q3.py
@@ -0,0 +1,8 @@
+num1=int(input("num1:"))
+num2=int(input("num2:"))
+print("Before swapping: num1 =", num1, " num2 =", num2)
+
+# swapping
+num1, num2 = num2, num1
+
+print("After swapping: num1 =", num1, " num2 =", num2)
